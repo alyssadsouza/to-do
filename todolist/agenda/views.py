5b8fff19a41bf1c88a182d0aba94d995bdee9de5
Alyssa D'Souza <alyssa.dsouza63@gmail.com>
2021-04-25 15:26:44 -0400

    Add 'add task' form

--- todolist/agenda/views.py
+++ todolist/agenda/views.py
@@ -8,9 +8,29 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='login')
 def home(request):
+    if request.method == 'POST':
+        old_tasks = list(request.POST.get("tasks")[2:-1])
+        tasks = []
+        i = old_tasks[1:].index("'")
+        while i < len(old_tasks):
+            task = ''.join(old_tasks[:i+1])
+            tasks.append(task)
+            old_tasks = old_tasks[i+5:]
+            if "'" in old_tasks:
+                i = old_tasks[1:].index("'")
+            else:
+                break
+        task = request.POST.get("task")
+        if task not in tasks and len(tasks) > 0:
+            tasks.append(task)
+    else:
+        tasks = ["task","other"]
     return render(request,'agenda/home.html', {
-        "tasks":['Wash Dishes', 'Fold Laundry']
+        "tasks":tasks,
+        "username":request.user
     })
+
+
 
 def loginPage(request):
     if request.method == 'POST':
